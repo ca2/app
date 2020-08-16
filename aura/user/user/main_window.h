9a29670006a6a2e7115e155f28fbca6a669b749f
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-08-16 03:16:52 -0300

    <3tbs, Mummi and bilbo!!
    sodapoppin and friends contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    -- hi5 contribution...!!

---
+++ aura/user/user/main_window.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+namespace user
+{
+
+
+   class CLASS_DECL_AURA main_window :
+      virtual public ::user::interaction
+   {
+   public:
+
+
+      main_window();
+      virtual ~main_window();
+
+
+      virtual void install_message_routing(::channel* pchannel) override;
+
+
+      virtual void on_create_user_interaction() override;
+
+   };
+
+
+
+} // namespace user
+
+
+

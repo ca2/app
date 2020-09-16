48a4790b60b3ee5549d236b68cbcabe20eb75fb2
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-09-16 16:46:19 -0300

    🌌 🌍 [ ca2 ] [ C++ ] macOS <3tbs, Mummi and bilbo!!
    sodapoppin and friends contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    -- hi5 contribution...!!
    
    from bilbo machine running macOS Catalina 10.15.6 with Supplemental Update!!

---
+++ aura/os/cross/_.h
@@ -0,0 +1,12 @@
+//
+//  _.h
+//  aura
+//
+//  Created by Camilo Sasuke Tsumanuma on 16/09/20.
+//
+
+#ifndef __h
+#define __h
+
+
+#endif /* __h */
---
+++ aura/os/cross/windows/windowing.h
@@ -0,0 +1,12 @@
+//
+//  windowing.h
+//  aura
+//
+//  Created by Camilo Sasuke Tsumanuma on 16/09/20.
+//
+
+#ifndef windowing_h
+#define windowing_h
+
+
+#endif /* windowing_h */

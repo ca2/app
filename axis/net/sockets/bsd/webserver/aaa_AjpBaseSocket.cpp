5ca6856b17482c7446b2ba3e4ceb5c9815a0fc29
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-10-04 19:01:55 -0300

    <3tbs, Mummi and bilbo!!
    jitspoe and strager streaming contribution!!
    
    jhoovgaard streaming (Big Ambitions game development) contribution!!
    
    GexYT streaming contribution!!
    
    Ash_F0x streaming contribution!!
    
    Jaxstyle streaming contribution!!
    
    cupsarestupid chatting contribution!!
    
    Trainwreckstv & Friend streaming contribution!!
    
    ShemicalX and Friend streaming contribution!!
    
    BarRaider streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    TheCherno streaming contribution!!
    
    rw_grin and FearItSelf(&Friends) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    AnderssonKev and JessicaMak streaming contribution!!
    
    TimBeaudet streaming contribution!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Greekgodx (&Friends) & FletcherLabs streaming contribution (at September: His month ? (a party ?) at least for me, just Don't wake me up for nothing!!) (Saturday Dawn / Morning)!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    -----
    from bilbo running macOS Catalina 10.15.6 with Supplemental Update

--- axis/net/sockets/bsd/webserver/aaa_AjpBaseSocket.cpp
+++ axis/net/sockets/bsd/webserver/aaa_AjpBaseSocket.cpp
@@ -21,7 +21,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "framework.h"
-#include "aura/net/sockets/_.h"
+#include "apex/net/sockets/_.h"
+#include "axis/net/sockets/bsd/webserver/ajp13.h"
+#include "axis/net/sockets/bsd/webserver/ajp_base_socket.h"
 
 
 namespace sockets
--- axis/platform/aura_str_pool.cpp
+++
@@ -1,8 +0,0 @@
-#include "framework.h"
-#include "aura/astr.h"
-
-
-CLASS_DECL_AXIS aura_str_pool* s_paurastrpool = nullptr;
-
-
-

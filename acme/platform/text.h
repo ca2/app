6f98acc69716f2fa9100de80f3dc84ab6a028842
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-10-13 01:41:04 -0300

    Update Utility at macOS
    <3tbs, Mummi and bilbo!!
    
    EmptyGroceryBag (Joey) chatting after long time contribution!!
    
    alfredotigolo chatting contribution!!
    
    Endesga C++ Development streaming  contribution!!
    
    tomrandall streaming contribution!!
    
    valdinei chatting contributtion after a long time!!
    
    P4ndaExpress (Dunno) chatting contribution!!
    
    SeppyTw and vue_equalizer (fpga_guru) talking contribution!!
    
    CeeJaey streaming contribution (Mario 3D)!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    P4andaExpress (Dunno) and vue_equalizer (fpgaguru) chatting contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    GexYT streaming contribution!!
    
    Mads Kristensen@Visual Studio streaming contribution!!
    
    JessicaMak, BendixBoy (& Friend) and AnderssonKev streaming contribution!!
    
    strager and TheCherno streaming contribution!!
    
    eren_kaynar7 chatting contribution!!
    
    vue_equalizer (fpgaguru) chatting and ca2 development testing contribution!!
    
    jhovgaard and tek256 streaming contribution!!
    
    Ninja, Melchizedeck6809, Vimlark and strager streaming contribution!!
    
    TimBeaudet and RareBytes (& friend) streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    ShemicalX streaming contribution!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    Ash_F0x and Myrkee streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    phanxgames and brianretrocorner streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    jitspoe streaming contribution!!
    
    -- hi5 contribution...!!
    
    bilbo machine running Catalina 10.15.6

---
+++ acme/platform/text.h
@@ -0,0 +1,13 @@
+//
+//  text.h
+//  acme
+//
+//  Created by Camilo Sasuke Tsumanuma on 12/10/20.
+//  Copyright © 2020 Camilo Sasuke Tsumanuma. All rights reserved.
+//
+
+#ifndef text_h
+#define text_h
+
+
+#endif /* text_h */

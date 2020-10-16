37fdbd99f6a3e7dd61796655faee9fff12512d06
Camilo Sasuke <camilo@ca2.email>
2020-10-16 15:32:19 -0300

    moved m_pobjectContext from element to context_object
    <3tbs, Mummi and bilbo!!
    
    EmptyGroceryBag (Joey) chatting after long time contribution!!
    
    alfredotigolo chatting contribution!!
    
    Endesga C++ Development streaming  contribution!!
    
    phaxgames (Henri Price) streaming contribution!!
    
    OfficialAndyPyro streaming contribution!!
    
    nextTab_de streaming contribution!!
    
    Kebabskal streaming contribution!!
    
    ThatDanishGuy streaming contribution!!
    
    strawberryhacker streaming contribution!!
    
    tomrandall streaming contribution!!
    
    valdinei chatting contributtion after a long time!!
    
    P4ndaExpress (Dunno) chatting contribution!!
    
    SeppyTw and vue_equalizer (fpga_guru) talking contribution!!
    
    CeeJaey streaming contribution (Mario 3D)!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    P4andaExpress (Dunno) and vue_equalizer (fpgaguru) chatting contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    GexYT streaming contribution!!
    
    Mads Kristensen@Visual Studio streaming contribution!!
    
    JessicaMak, BendixBoy (& Friend) and AnderssonKev streaming contribution!!
    
    strager and TheCherno streaming contribution!!
    
    eren_kaynar7 chatting contribution!!
    
    vue_equalizer (fpgaguru) chatting and ca2 development testing contribution!!
    
    jhovgaard and tek256 streaming contribution!!
    
    DonHo streaming contribution!!
    
    nextTab_de streaming contribution!!
    
    emergoentertainment streaming contribution!!
    
    Ninja, Melchizedeck6809, Vimlark and strager streaming contribution!!
    
    TimBeaudet and RareBytes (& friend) streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    ShemicalX streaming contribution!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    Ash_F0x and Myrkee streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    phanxgames and brianretrocorner streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    jitspoe streaming contribution!!
    
    -- hi5 contribution...!!
    
    from matrix (netnode-canada-camilo-account) dedicated server running Windows 10 Pro Pro through OVH.pt!!

--- aura/_.cpp
+++ aura/_.cpp
@@ -74,145 +74,145 @@ string object_name(element* p)
 
 }
 
-#if OBJ_REF_DBG
-
-void element::add_ref_history(element* p, const char* pszObjRefDbg)
-{
-
-   if (!::acme::g_bAcme)
-   {
-
-      return;
-
-   }
-
-   cslock sl(&::acme::g_csRefDbg);
-
-   if (g_bEnableObjRefDbg)
-   {
-
-      g_bEnableObjRefDbg = false;
-
-      try
-      {
-
-         if (m_pobjrefdbg == nullptr)
-         {
-
-            m_pobjrefdbg = new obj_ref_dbg;
-
-         }
-
-         string strNote = ::object_name(p);
-
-         if (::is_set(pszObjRefDbg))
-         {
-
-            strNote += " - " + string(pszObjRefDbg);
-
-         }
-
-         m_pobjrefdbg->m_itema.add_item({ m_pobjrefdbg->m_iStep++, p, strNote });
-
-      }
-      catch (...)
-      {
-
-      }
-
-      g_bEnableObjRefDbg = true;
-
-   }
-
-}
-
-
-void element::dec_ref_history(element* p, const char* /*pszObjRefDbgNotUsedCurrently*/)
-{
-
-   cslock sl(&::acme::g_csRefDbg);
-
-   if (m_pobjrefdbg == nullptr || m_countReference <= 0 || m_pobjrefdbg->m_itema.isEmpty())
-   {
-
-      return;
-
-   }
-
-   ::index iFind = m_pobjrefdbg->m_itema.pred_find_last([p](auto& item) {return item.m_p == p; });
-
-   if (iFind >= 0)
-   {
-
-      m_pobjrefdbg->m_itema.remove_at(iFind);
-
-   }
-
-}
-
-
-void element::check_pending_releases()
-{
-
-   cslock sl(&::acme::g_csRefDbg);
-
-   if (m_pobjrefdbg == nullptr)
-   {
-
-      return;
-
-   }
-
-   if (m_pobjrefdbg->m_itema.has_element())
-   {
-
-      ::output_debug_string("\nThere are pending releases:\n");
-
-      try
-      {
-
-         ::output_debug_string("For "+__str(typeid(*this).name())+"("+string(debug_note())+")\n");
-
-      }
-      catch (...)
-      {
-
-      }
-
-      for (index i = 0; i < m_pobjrefdbg->m_itema.get_count(); i++)
-      {
-
-         auto& item = m_pobjrefdbg->m_itema[i];
-
-         ::index iStep = item.m_iStep;
-
-         ::element* pobj = item.m_p;
-
-         string str = item.m_strNote;
-
-         try
-         {
-
-            ::output_debug_string(__str(iStep) + ": " + str + "\n");
-
-         }
-         catch (...)
-         {
-
-
-         }
-
-      }
-
-      ::output_debug_string("-------------------\n\n");
-
-   }
-
-
-}
-
-#endif
-
+//#if OBJ_REF_DBG
+//
+//void element::add_ref_history(element* p, const char* pszObjRefDbg)
+//{
+//
+//   if (!::acme::g_bAcme)
+//   {
+//
+//      return;
+//
+//   }
+//
+//   cslock sl(&::acme::g_csRefDbg);
+//
+//   if (g_bEnableObjRefDbg)
+//   {
+//
+//      g_bEnableObjRefDbg = false;
+//
+//      try
+//      {
+//
+//         if (m_pobjrefdbg == nullptr)
+//         {
+//
+//            m_pobjrefdbg = new obj_ref_dbg;
+//
+//         }
+//
+//         string strNote = ::object_name(p);
+//
+//         if (::is_set(pszObjRefDbg))
+//         {
+//
+//            strNote += " - " + string(pszObjRefDbg);
+//
+//         }
+//
+//         m_pobjrefdbg->m_itema.add_item({ m_pobjrefdbg->m_iStep++, p, strNote });
+//
+//      }
+//      catch (...)
+//      {
+//
+//      }
+//
+//      g_bEnableObjRefDbg = true;
+//
+//   }
+//
+//}
+//
+//
+//void element::dec_ref_history(element* p, const char* /*pszObjRefDbgNotUsedCurrently*/)
+//{
+//
+//   cslock sl(&::acme::g_csRefDbg);
+//
+//   if (m_pobjrefdbg == nullptr || m_countReference <= 0 || m_pobjrefdbg->m_itema.isEmpty())
+//   {
+//
+//      return;
+//
+//   }
+//
+//   ::index iFind = m_pobjrefdbg->m_itema.pred_find_last([p](auto& item) {return item.m_p == p; });
+//
+//   if (iFind >= 0)
+//   {
+//
+//      m_pobjrefdbg->m_itema.remove_at(iFind);
+//
+//   }
+//
+//}
+//
+//
+//void element::check_pending_releases()
+//{
+//
+//   cslock sl(&::acme::g_csRefDbg);
+//
+//   if (m_pobjrefdbg == nullptr)
+//   {
+//
+//      return;
+//
+//   }
+//
+//   if (m_pobjrefdbg->m_itema.has_element())
+//   {
+//
+//      ::output_debug_string("\nThere are pending releases:\n");
+//
+//      try
+//      {
+//
+//         ::output_debug_string("For "+__str(typeid(*this).name())+"("+string(debug_note())+")\n");
+//
+//      }
+//      catch (...)
+//      {
+//
+//      }
+//
+//      for (index i = 0; i < m_pobjrefdbg->m_itema.get_count(); i++)
+//      {
+//
+//         auto& item = m_pobjrefdbg->m_itema[i];
+//
+//         ::index iStep = item.m_iStep;
+//
+//         ::element* pobj = item.m_p;
+//
+//         string str = item.m_strNote;
+//
+//         try
+//         {
+//
+//            ::output_debug_string(__str(iStep) + ": " + str + "\n");
+//
+//         }
+//         catch (...)
+//         {
+//
+//
+//         }
+//
+//      }
+//
+//      ::output_debug_string("-------------------\n\n");
+//
+//   }
+//
+//
+//}
+//
+//#endif
+//
 
 lparam_dbg g_lparamdbg;
 

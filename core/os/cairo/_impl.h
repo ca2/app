c34170607c54a9c2d4566ea316280414ad5577a9
Camilo Sasuke <camilo@ca2.email>
2020-11-01 15:45:16 -0300

    [Linux/Manjaro] Porting
    <3tbsThomasBS_ [ THE MAN ] [ ABSOLUTE ] [ GOD ], Mummi and bilbo!!
    
    Watching Thomas [ THE GOD ] video at You Tube!!
    
    EmptyGroceryBag (Joey) chatting after long time contribution!!
    
    alfredotigolo chatting contribution!!
    
    Endesga C++ Development streaming  contribution!!
    
    TommyLuco chatting recalling <3tbs!!
    
    OfficialAndyPyro, JaxStyle, briansretrocorner, EduardoRFS, ShortByte, Promic2 and heroseh streaming contribution!!
    
    tomrandall streaming contribution!!
    
    valdinei chatting contributtion after a long time!!
    
    fpga_guru chatting contribution!!
    
    nybblesio streaming contribution!!
    
    MartilloWorkshop streaming contribution!!
    
    ChristianParpart streaming contribution!!
    
    P4ndaExpress (Dunno) chatting contribution!!
    
    SeppyTw and vue_equalizer (fpga_guru) talking contribution!!
    
    CeeJaey streaming contribution (Mario 3D)!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    Myrkee streaming contribution!!
    
    EduardoRFS streaming and raid contribution!!
    
    marcobrunodev streaming contribution!!
    
    P4andaExpress (Dunno) and vue_equalizer (fpgaguru) chatting contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    GexYT streaming contribution!!
    
    Mads Kristensen@Visual Studio streaming contribution!!
    
    JessicaMak, BendixBoy (& Friend) and AnderssonKev streaming contribution!!
    
    strager and TheCherno streaming contribution!!
    
    eren_kaynar7 chatting contribution!!
    
    vue_equalizer (fpgaguru) chatting and ca2 development testing contribution!!
    
    jhovgaard and tek256 streaming contribution!!
    
    DonHo streaming contribution!!
    
    nextTab_de streaming contribution!!
    
    emergoentertainment streaming contribution!!
    
    Ninja, Melchizedeck6809, Vimlark and strager streaming contribution!!
    
    TimBeaudet and RareBytes (& friend) streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    ShemicalX streaming contribution!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    Okbatgames chatting contribution!!
    
    Ash_F0x and Myrkee streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    phanxgames and brianretrocorner streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    jitspoe streaming contribution!!
    
    -- hi5 contribution...!!
    
    Manjaro GNOME 20.1.2 VMware Guest at canada netnode running Windows 10 Pro remotely from bilbo machine running Windows 10 Pro

--- core/os/cairo/_impl.h
+++ core/os/cairo/_impl.h
@@ -1,27 +1,27 @@
 #pragma once
 
 
-inline void copy(RECT * prectTarget, const cairo_rectangle_int_t * prectSource)
-{
-
-   prectTarget->left = prectSource->x;
-   prectTarget->top = prectSource->y;
-   prectTarget->right = prectSource->x + prectSource->width;
-   prectTarget->bottom = prectSource->y + prectSource->height;
-
-}
-
-
-inline void copy(cairo_rectangle_int_t * prectTarget, const RECT * prectSource)
-{
-
-   prectTarget->x = prectSource->left;
-   prectTarget->y = prectSource->top;
-   prectTarget->width = width(prectSource);
-   prectTarget->height = height(prectSource);
-
-
-}
+//inline void copy(RECT * prectTarget, const cairo_rectangle_int_t * prectSource)
+//{
+//
+//   prectTarget->left = prectSource->x;
+//   prectTarget->top = prectSource->y;
+//   prectTarget->right = prectSource->x + prectSource->width;
+//   prectTarget->bottom = prectSource->y + prectSource->height;
+//
+//}
+//
+//
+//inline void copy(cairo_rectangle_int_t * prectTarget, const RECT * prectSource)
+//{
+//
+//   prectTarget->x = prectSource->left;
+//   prectTarget->y = prectSource->top;
+//   prectTarget->width = width(prectSource);
+//   prectTarget->height = height(prectSource);
+//
+//
+//}
 
 
 

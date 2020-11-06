2d13d6e0ea0765e5c14b6dfc4e908c057eb72fd6
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-11-06 09:51:26 -0300

    New Source Tree Structure
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    -- hi5 contribution...!!
    
    from eddie machine running Windows 10 Pro remotely from bilbo machine running Windows 10 Pro!!

---
+++ acme/primitive/primitive/procedure.cpp
@@ -0,0 +1,24 @@
+#include "framework.h"
+
+
+//method & method::operator = (const ::var& var)
+//{
+//   
+//   if (var.get_type() == ::type_method)
+//   {
+//
+//      m_pmatter = var.m_method.m_pmatter;
+//
+//   }
+//   else
+//   {
+//
+//      m_pmatter.release();
+//
+//   }
+//
+//   return *this;
+//
+//}
+
+

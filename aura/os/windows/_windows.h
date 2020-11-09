1f3c1c1881168ae715aebc4af3d08538213a4ed1
Camilo Sasuke <camilo@ca2.email>
2020-11-08 22:28:19 -0300

    [Windows Desktop] New Source Tree Structure -  CMake and CLion as Build/Development Alternative
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    boflux (Spoofh, Benjamin Kuhl) chatting contribution!!
    
    fpga_guru (vue_equalizer, Oliver Pohl) chatting contribution!!
    
    cmgriffing streaming contribution!!
    
    TimBeaudet (Friends: FletcherLabs, tsjost and Jabokoe) streaming contribution!!
    
    Stumpen_nicklas_dk, sodapopping and EduardoRFS streaming contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    -- hi5 contribution...!!
    
    from eddie machine running Windows 10 Pro remotely from bilbo machine running Windows 10 Pro!!

--- aura/os/windows/_windows.h
+++ aura/os/windows/_windows.h
@@ -1,22 +1,5 @@
 #pragma once
 
 
-#include <fcntl.h>
-#include <VersionHelpers.h>
-#include <ddeml.h>
-#include <share.h>
-#include <io.h>
-#include <process.h>
-#include <Winsvc.h>
-
-
-#include "exception_engine.h"
-
-
-#include "dir.h"
-
-
-//#include "extract_icon.h"
-
 
 

09d2242de81fb631b942e03bf06796d1a914d36f
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-11-14 11:45:05 -0300

    🌌 🌍 [Windows] New Source Tree Structure : CMake and CLion as Build/Development Alternative

---
+++ aura/graphics/draw2d/_const.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+namespace draw2d
+{
+
+
+   namespace enum_interpolation_mode
+   {
+
+      e_interpolation_mode_none = 0,
+      e_interpolation_mode_fast,
+      e_interpolation_mode_low_quality,
+      e_interpolation_mode_high_quality,
+
+   }
+
+
+} // namespace draw2d
+
+
+

aaa567dea4f5e680ce71f689a40e4104a4eec188
Camilo Sasuke Tbs_ <camilosasuketbs@ca2.email>
2020-11-19 07:41:31 -0300

    [Manjaro20.1.2/Linux] New Source Tree Structure / CMake and CLion as Build/Development Alternative
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    cmgriffing streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    -- hi5 contribution...!!
    
    at Manjaro 20.1.2 Dual Boot at eddie machine through Anydesk from bilbo running Windows 10 Pro!!

--- draw2d_cairo/image_impl.h
+++ draw2d_cairo/image_impl.h
@@ -11,12 +11,6 @@ namespace draw2d_cairo
    public:
 
 
-      //::draw2d::bitmap_pointer      m_pbitmap;
-      //::draw2d::graphics_pointer    m_spgraphics;
-      //bool                          m_bTrans;
-
-
-//      class size64            m_sizeWnd;
 #ifdef WINDOWS
       HBITMAP                 m_hbitmap;
 //      BITMAPINFO              m_bitmapinfo;
@@ -25,7 +19,6 @@ namespace draw2d_cairo
 
 
       image();
-      //virtual void construct(i32 cx, i32 cy);
       virtual ~image();
 
 
@@ -37,43 +30,26 @@ namespace draw2d_cairo
       virtual ::draw2d::bitmap_pointer get_bitmap() const override;
       virtual ::draw2d::bitmap_pointer detach_bitmap() override;
 
-
-      //virtual color32_t * get_data();
-
-      virtual bool stretch_image(::image * pimage_impl) override;
+      virtual bool stretch(::image * pimage_impl) override;
 
       bool dc_select(bool bSelect = true) override;
 
-
-
-
-
       virtual ::estatus create(const ::size & size, ::eobject eobjectCreate = OK, int iGoodStride = -1, bool bPreserve = false) override;
-      //virtual bool create(i32 iWidth, i32 iHeight, const ::uid & uidCreateImage = OK, int iGoodStride = -1) override;
       virtual ::estatus create(::draw2d::graphics * pgraphics) override;
       virtual bool destroy() override;
 
 
-virtual bool on_host_read_pixels(const ::pixmap * ppixmap) override;
+      virtual bool on_host_read_pixels(const ::pixmap * ppixmap) override;
       bool from(::draw2d::graphics * pgraphics) override;
-      //bool from(const ::point & ptDest, ::draw2d::graphics * pgraphics, const ::point & point, const ::size & size) override;
 
       bool to(::draw2d::graphics * pgraphics, const ::point & point, const ::size & size, const ::point & pointSrc) override;
-//#if defined(WINDOWS) || defined(LINUX) || defined(ANDROID)
-  //    virtual bool update_window(::aura::draw_interface * pwnd,::message::message * pmessage, bool bTransferBuffer);
-    //  virtual bool print_window(::aura::draw_interface * pwnd,::message::message * pmessage);
-//#endif
       virtual ::estatus SetIconMask(::draw2d::icon * picon, i32 cx, i32 cy) override;
-
-
-      //virtual void io(stream & stream) override;
 
 
       virtual ::stream & write(::stream & stream) const override;
       virtual ::stream & read(::stream & stream) override;
 
 
-      //virtual bool blend(point ptDst, ::image * pimage, point ptSrc, ::size size, byte bA) override;
       virtual bool blend(const ::point & ptDst, ::image * pimageAlf, const ::point & pointAlf, const ::size & size, byte bA) override;
 
 

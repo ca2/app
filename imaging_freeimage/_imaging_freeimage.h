27443668ed0ee73279657baafcac58a1b437e59e
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@ca2.email>
2020-12-30 02:00:50 -0300

    [Ubuntu20.10/Linux] Stabilization
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN ABSOLUTE <3!!
    
    Mummi late dawn (till 6:10AM Oct5) presence contribution!!
    
    bilbo requesting food and to be together (5:10AM Oct5) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe (till 18:10? Oct5), Endesga and Fearitself streaming contribution!!
    
    tomrandall (till 19:10 Oct5) streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    bytakos streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    -- hi5 contribution...!!
    
    at Ubuntu 20.10 at bilbo machine remotely from bilbo machine running Windows 10 Pro!!

---
+++ imaging_freeimage/_imaging_freeimage.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+#ifdef LINUX
+
+#undef _WINDOWS_
+
+#include <FreeImage.h>
+
+#else
+
+#include "FreeImage/FreeImage.h"
+
+#endif
+
+
+namespace imaging_freeimage
+{
+
+
+   CLASS_DECL_IMAGING_FREEIMAGE FIBITMAP * freeimage_from_image(const ::image * pimage);
+   CLASS_DECL_IMAGING_FREEIMAGE bool image_from_freeimage(::image * pimage, FIBITMAP * pfibitmap);
+
+
+} // namespace imaging_freeimage
+
+#include "fimemory.h"
+

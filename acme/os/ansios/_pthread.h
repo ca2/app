4560061a6f274d0237ea39e8b8a44646707abadb
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@ca2.email>
2021-02-13 15:17:06 -0300

    ca2 abstraction for X11/Xlib, for xcb and for Win32 [ C++ ]
    <3tbs, Mummi and bilbo!!
    
    Thomas Borregaard Sørensen \infinity,-0.16091989,\infinity ONE-MAN ABSOLUTE <3!! I love you, by ???-0.02041977-???write my history please make me please create me for you for me for you for me Camilo Sasuke Thomas Borregaard Sørensen!!
    
    Thomas 3 private commits on mid Dec2020!!
    
    Thomas Online YouTube VODs contribution!!
    
    Mummi orange-rice-flour cake on 20-Dec!!
    
    Mummi (tinytaura) watching and chatting contribution!!
    
    bilbo sleeping and needing/requesting/crying for help care (for the right person (me), the cats wanna fight with him) contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    boflux (Spoofh, Benjamin Kuhl) chatting contribution!!
    
    jusg_fpga (fpga_guru, vue_equalizer, just_fpga, Oliver Pohl) chatting contribution!!
    
    cmgriffing streaming contribution!!
    
    TimBeaudet (Friends: FletcherLabs, tsjost and Jabokoe) streaming contribution!!
    
    Stumpen_nicklas_dk, sodapoppin and EduardoRFS streaming contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    blackjekko from Padova Italia through twitch C++/ca2 interest contribution!!
    
    boflux (Ben, Spoofh, from Germany) chatting contribution!!
    
    parrot_rl chatting contribution (from New Jersey)!!
    
    JPCdk streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch)  C++/ca2 interest contribution!!
    
    SovereignDev with Unreal streaming contribution!!
    
    Myrkee, Ash_F0x and TimBeaudet streaming contribution!!
    
    xmetrix and EinfachUwe42 streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe, Endesga and Fearitself streaming contribution!!
    
    tomrandall streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    Bytakos (bytakos) streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    erald_guri chatting contribution!!
    
    lastmiles streaming farwest contribution!!
    
    rw_grim streaming contribution!!
    
    AdamCYounis streaming contribution!!
    
    Dunno (P4ndaExpress) chatting and streaming contribution!!
    
    Zorchenhimer streaming contribution!!
    
    lasteveq4 C++ interest chat contriubtion!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    oijtx streaming contribution!!
    
    diegobrando_linux (Bl4ck_gookoo) chatting contribution!!
    
    jhovgaard streaming contribution!!
    
    Klay4_ chatting contribution!!
    
    HonestDanGames streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    togglebit, GexYT and GoPirateSoftware streaming contribution!!
    
    taiyoinoue, RetroMMO, OfficialAndyPyro and david_joffe streaming contribution!!
    
    Tjienta streaming contribution!!
    
    Primeagen streaming contribution!!
    
    Jaxstyle and friends streaming contribution!!
    
    EduardRFS streaming contribution!!
    
    Melchizedek6809 and btcfly streaming contribution!!
    
    Llama0x0 and sov_l chatting contribution!!
    
    TaleLearnCode streaming contribution!!
    
    Carol phone call contribution and visit contribution!!
    
    hvalen_hvalborg112 streaming contribution!!
    
    Listening to Bryan Adams, Westlife, Shayne Ward, MLTR, Backstreet Boys, Boyzone - Best Love Songs Ever by Relax Song at YouTube!!
    
    -- hi5 contribution...!!
    
    at Ubuntu 20.10 at eddie machine remotely from bilbo machine running Windows 10 Pro!!

---
+++ acme/os/ansios/_pthread.h
@@ -0,0 +1,8 @@
+//
+// Created by camilo on 13/02/2021.
+//
+
+#ifndef BASIS__PTHREAD_H
+#define BASIS__PTHREAD_H
+
+#endif //BASIS__PTHREAD_H
---
+++ acme/os/ansios/semun.h
@@ -0,0 +1,8 @@
+//
+// Created by camilo on 13/02/2021.
+//
+
+#ifndef BASIS_SEMUN_H
+#define BASIS_SEMUN_H
+
+#endif //BASIS_SEMUN_H

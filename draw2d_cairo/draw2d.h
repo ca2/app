49d94dc59dcfd5694e1ad68f6f706ef9747c79c3
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@ca2.email>
2021-02-18 22:28:43 -0300

    ca2 windowing for *X11/Xlib*, for xcb and for *Win32* [ C++ ]
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN absolute <3!!
    
    Thomas VODs!!
    
    Mummi late dawn (till 6:10 (AM)) presence contribution!!
    
    Mummi taking care of Home and Belly and Body and Health!!
    
    bilbo requesting to enter IT room (past 4:10 (AM)) and laying on his tapestry contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe, Endesga and Fearitself streaming contribution!!
    
    tomrandall streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru, just_fpga, Oliver Pohl) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    polyglot_developer C++/ca2 interest contribution!!
    
    blackjekko@twitch(IT) C++/ca2 interest contribution!!
    
    devdecrux C++/ca2 interest contribution!!
    
    yerayp10 chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    Goldbargames streaming contribution!!
    
    Dunno (P4ndaExpress, P4ndamicMoon) chatting and C++/ca2 interest contribution!!
    
    whyyyyyyysoserious (Mark Esserman) streaming contribution!!
    
    thebaker__ streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    jmcmorris (Jason Morris, SiegeGames) streaming contribution!!
    
    erald_guri chatting contribution!!
    
    j_blow streaming contribution!!
    
    lasteveq4 C++ interest chat contribution!!
    
    EingachUwe42 streaming contribution!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    cmgriffing streaming contribution!!
    
    oijtx streaming contribution!!
    
    Tectuma Crew (GothicCat1 (Stacy), Tectuma (Kevin) and Nick) streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    KASPERPURE (Dan/Eng) Mario64 streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    TheCherno streaming contribution!!
    
    -- hi5 contribution...!!
    
    from bilbo machine running Windows 10 Pro!!

---
+++ draw2d_cairo/draw2d.h
@@ -0,0 +1,112 @@
+#pragma once
+
+
+namespace draw2d
+{
+
+
+   class CLASS_DECL_AURA draw2d  :
+      virtual public ::apex::department
+   {
+   public:
+
+
+      static double g_dEmboss;
+
+
+      i32_map < __pointer(memory) > m_alpha_spread__24CC_filterMap;
+      i32_map < __pointer(memory) > m_alpha_spread__32CC_filterMap;
+
+
+      __pointer(class ::draw2d::api)                           m_papi;
+
+      //::mutex                                        m_pmutexFont;
+//#ifdef WINDOWS
+  //    __composite(::draw2d_direct2d::plugin)                     m_pplugin;
+//#endif
+
+      __pointer(::write_text::write_text)                      m_pwritetext;
+
+
+
+      draw2d();
+      virtual ~draw2d();
+
+
+      virtual ::e_status initialize(::layered * pobjectContext) override;
+
+      virtual ::e_status process_init() override;
+
+      virtual ::e_status init() override;
+      virtual ::e_status init1() override;
+
+      virtual void term() override;
+
+      virtual void finalize() override;
+
+
+      virtual class api & api();
+
+      inline class ::write_text::write_text * write_text() { return m_pwritetext; }
+
+//#ifdef WINDOWS
+  //    ::draw2d_direct2d::plugin * direct2d();
+//#endif
+
+      
+
+      e_format file_extension_to_format(const ::payload & varFile);
+      e_format text_to_format(string str);
+      
+      virtual ::e_status initialize_write_text();
+      virtual ::e_status write_text_factory_exchange(::factory_map * pfactorymap);
+
+      virtual void term_instance() override;
+
+      virtual string write_text_get_default_library_name();
+
+      virtual bool embossed_text_out(
+      ::draw2d::graphics_pointer & pgraphics,
+      const ::rectangle_i32 & rectangle,
+      string strText,
+      ::draw2d::fastblur & blur,
+      ::image_pointer & imageBlur,
+      ::write_text::font * pfont,
+      const ::e_align & ealign,
+      const ::e_draw_text & edrawtext,
+      color32_t crText,
+      color32_t crGlow,
+      int iSpreadRadius,
+      int iBlurRadius,
+      int iBlur,
+      bool bUpdate,
+      const ::color_filter & colorfilter = nullptr);
+
+
+      template < typename PRED >
+      bool emboss_predicate(
+      ::draw2d::graphics_pointer & pgraphics,
+      const ::rectangle_i32 & rectangle,
+      PRED pred,
+      ::draw2d::fastblur & blur,
+      ::image_pointer & imageBlur,
+      color32_t crGlow,
+      int iSpreadRadius,
+      int iBlurRadius,
+      int iBlur,
+      bool bUpdate,
+      const color_filter & colorfilter = nullptr);
+
+      virtual void alpha_spread__24CC(byte * pbDest, i32 xDest, i32 yDest, i32 wDest, i32 cx, i32 cy, byte * pbSrc, i32 ySrc, i32 xSrc, i32 wSrc, byte bMin, i32 iRadius);
+
+      virtual bool channel_spread__32CC(::image * pimageDst, ::image * pimageSrc, i32 iChannel, i32 iRadius, color32_t crSpreadSetColor);
+
+      //virtual void enum_draw2d_fonts(::write_text::font_enum_item_array& itema);
+
+
+   };
+
+
+} // namespace draw2d
+
+
---
+++ node_ansios/datetime.h
@@ -0,0 +1,8 @@
+//
+// Created by camilo on 16/02/2021.
+//
+
+#ifndef BASIS_DATETIME_H
+#define BASIS_DATETIME_H
+
+#endif //BASIS_DATETIME_H

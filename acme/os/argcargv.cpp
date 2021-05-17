3e6b57d2753a06568556335f2d8c6ff418b6ca84
Camilo Sasuke Thomas Borregaard S?rensen <camilosasuketbs@gmail.com>
2021-05-16 21:15:28 -0300

    Porting to Windows
    <3tbs, Mummi and bilbo!!
    
    Thomas ONE-MAN absolute <3!!
    
    Thomas VODs!!
    
    Mummi late dawn (till 6:10 (AM)) presence contribution!!
    
    Mummi taking care of Home and Belly and Body and Health!!
    
    bilbo requesting to enter IT room (past 4:10 (AM)) and laying on his tapestry contribution!!
    
    sodapoppin and friends contribution!!
    
    iAssyrian chatting contribution!!
    
    alfredotigolo, mandrakenk and Okbatgames chatting contribution!!
    
    jitspoe, Endesga and Fearitself streaming contribution!!
    
    tomrandall streaming Ludum contribution!!
    
    vue_equalizer (fpga_guru, just_fpga, Oliver Pohl) chatting contribution!!
    
    Thiagovgamg chatting contribution!!
    
    polyglot_developer C++/ca2 interest contribution!!
    
    blackjekko@twitch(IT) C++/ca2 interest contribution!!
    
    devdecrux C++/ca2 interest contribution!!
    
    yerayp10 chatting contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    普卡 (puka_) chatting contribution!!
    
    Goldbargames streaming contribution!!
    
    Dunno (p4ndawastaken, P4ndaExpress, P4ndamicMoon, a.k.a. AndreasPedersen@Denmark) chatting and C++/ca2 interest contribution!!
    
    whyyyyyyysoserious (Mark Esserman) streaming contribution!!
    
    thebaker__ streaming contribution!!
    
    Endesga streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Ash_F0x and JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    Ninja and friends streaming contribution!!
    
    Crinkem (from Australia) chatting, streaming and support contribution!!
    
    jmcmorris (Jason Morris, SiegeGames) streaming contribution!!
    
    erald_guri123 chatting contribution!!
    
    j_blow streaming contribution!!
    
    lasteveq4 C++ interest chat contribution!!
    
    EingachUwe42 streaming contribution!!
    
    cecilphillip and clarkio @"Microsoft Developer" streaming contribution!!
    
    cmgriffing streaming contribution!!
    
    oijtx streaming contribution!!
    
    Tectuma Crew (GothicCat1 (Stacy), Tectuma (Kevin) and Nick) streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    KASPERPURE (Dan/Eng) Mario64 streaming contribution!!
    
    Trainwreckstv and friends streaming contribution!!
    
    TheCherno streaming contribution!!
    
    -- hi5 contribution...!!
    
    from bilbo machine running Windows 10 Pro!!

--- acme/os/argcargv.cpp
+++ acme/os/argcargv.cpp
@@ -8,104 +8,10 @@
 
 #include "framework.h"
 
-#ifdef WINDOWS_DESKTOP
 
-#define _MAX_CMD_LINE_ARGS  32
-wchar_t *_argv[_MAX_CMD_LINE_ARGS+1];
-static wchar_t *_rawCmd = 0;
+//wchar_t* _argv[_MAX_CMD_LINE_ARGS + 1];
+//static wchar_t* _rawCmd = 0;
 
-i32 _init_args()
-{
-	_argv[0] = 0;
-
-	wchar_t *sysCmd = GetCommandLineW();
-	auto szSysCmd = wcslen(sysCmd);
-
-	// copy the system command line
-	WCHAR *cmd = (wchar_t*)HeapAlloc(GetProcessHeap(), 0, sizeof(char)*(szSysCmd+1));
-	_rawCmd = cmd;
-	if (!cmd)
-		return 0;
-	wcscpy(cmd, sysCmd);
-
-	// Handle a quoted filename
-	if (*cmd == _T('"'))
-	{
-		cmd++;
-		_argv[0] = cmd;						// argv[0] = exe name
-
-		while (*cmd && *cmd != _T('"'))
-			cmd++;
-
-		if (*cmd)
-			*cmd++ = 0;
-		else
-			return 0;						// no end quote!
-	}
-	else
-	{
-		_argv[0] = cmd;						// argv[0] = exe name
-
-		while (*cmd && !ansi_char_is_space(*cmd))
-			cmd++;
-
-		if (*cmd)
-			*cmd++ = 0;
-	}
-
-	i32 argc = 1;
-	for (;;)
-	{
-		while (*cmd && ansi_char_is_space(*cmd))		// Skip over any whitespace
-			cmd++;
-
-		if (*cmd == 0)						// End of command line?
-			return argc;
-
-		if (*cmd == _T('"'))					// Argument starting with a quote???
-		{
-			cmd++;
-
-			_argv[argc++] = cmd;
-			_argv[argc] = 0;
-
-			while (*cmd && *cmd != _T('"'))
-				cmd++;
-
-			if (*cmd == 0)
-				return argc;
-
-			if (*cmd)
-				*cmd++ = 0;
-		}
-		else
-		{
-			_argv[argc++] = cmd;
-			_argv[argc] = 0;
-
-			while (*cmd && !ansi_char_is_space(*cmd))
-				cmd++;
-
-			if (*cmd == 0)
-				return argc;
-
-			if (*cmd)
-				*cmd++ = 0;
-		}
-
-		if (argc >= _MAX_CMD_LINE_ARGS)
-			return argc;
-	}
-}
-
-void _term_args()
-{
-	if (_rawCmd)
-		HeapFree(GetProcessHeap(), 0, _rawCmd);
-}
-
-
-#endif
 
 
 

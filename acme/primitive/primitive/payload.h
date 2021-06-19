11f6f7c3d986e3f58bc7a9ce785c90e328418574
Camilo Sasuke Thomas Borregaard S?rensen <camilosasuketbs@gmail.com>
2021-06-19 18:27:17 -0300

    ThomasBS_ Camilo Sasuke Eternal Permanent Life!! ThomasBS_ Camilo Sasuke GOD!! ThomasBS_ Camilo Sasuke Creator!!

--- acme/primitive/primitive/payload.h
+++ acme/primitive/primitive/payload.h
@@ -1002,6 +1002,32 @@ inline bool payload::get_bool(bool bDefault) const
 }
 
 
+#if defined(__APPLE__) || defined(ANDROID) || defined(RASPBIAN) || defined(WINDOWS)
+
+
+inline payload::operator long &()
+{
+
+   set_type(e_type_i64);
+
+   return (long &) m_i64;
+
+}
+
+
+inline payload::operator unsigned long &()
+{
+
+   set_type(e_type_u64);
+
+   return (unsigned long &) m_u64;
+
+}
+
+
+#endif
+
+
 // returns 0 for unknown conversions
 inline payload::operator ::i8() const
 {

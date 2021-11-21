2fa0f89eba8f99b4e4502fa6b24c1195d9ea58cf
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2021-11-21 02:01:15 -0300

    <3Tbs!! - FreeBSD - CLion Laptop Box

---
+++ acme/node/operating_system/freebsd/_freebsd.h
@@ -0,0 +1,33 @@
+
+
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+
+#include <sys/stat.h>
+#include <fcntl.h>
+//#include <sys/inotify.h>
+
+
+#include <fcntl.h>
+#include <sys/file.h>
+#undef USE_MISC
+
+
+#include <dirent.h>
+
+
+platform_char *** process_get_pargv();
+platform_char ** process_get_argv();
+
+
+//#include "_user.h"
+
+//#include "desktop_file.h"
+//#include "x11.h"
+
+
+#include "exception_engine.h"
+
+
+

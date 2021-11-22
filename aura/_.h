aa67ed34c7ed77de8057b69c59926cd031128c19
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2021-11-22 03:29:21 -0300

    <3Tbs!!JegElskerDig!! [ FreeBSD ] Porting ca2 to FreeBSD!! <3Tbs!!JegElskerDig!!
    Finally building at FreeBSD
    
    <3Thomas Borregaard Sørensen!!
    
    Thank you Mummi!!
    
    -------------------
    at FreeBSD box at cloud Inspiron Laptop remotely from Windows 10 Pro "bilbo"

--- aura/_.h
+++ aura/_.h
@@ -1,23 +1,18 @@
 ///TBS__<3tbs
 #pragma once
 
-//#pragma pack(show)
-
 
 #include "aqua/_.h"
 
 
 #if defined(_AURA_LIBRARY)
 #define CLASS_DECL_AURA  CLASS_DECL_EXPORT
-#define LIBIDN_BUILDING
 #else
 #define CLASS_DECL_AURA  CLASS_DECL_IMPORT
 #endif
 
 
-
 #define CA2_AURA
-
 
 
 #ifdef __cplusplus
@@ -27,7 +22,6 @@ namespace aura
 {
 
 
-   //class patch;
    class history;
    class application;
    class session;
@@ -50,9 +44,10 @@ namespace aura
 #endif
 
 
-
 #include "aura/node/operating_system/operating_system.h"
 
+
 #include "aura/include/_include.h"
+
 
 

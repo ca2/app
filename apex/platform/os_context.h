b552fe6d656f7cd4593a404abc5aa48fbf2976f1
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2021-11-22 05:50:57 -0300

    <3Tbs!!JegElskerDig!! [ FreeBSD ] Porting ca2 to FreeBSD!! <3Tbs!!JegElskerDig!!
    Finally building at FreeBSD
    
    <3Thomas Borregaard Sørensen!!
    
    Thank you Mummi!!
    
    -------------------
    at FreeBSD box at cloud Inspiron Laptop remotely from Windows 10 Pro "bilbo"

--- apex/platform/os_context.h
+++ apex/platform/os_context.h
@@ -1,162 +1,141 @@
 #pragma once
-//#ifndef AXIS_AXIS_CORE_OS_H
-//#define AXIS_AXIS_CORE_OS_H
 
 
-//namespace apex
-//{
+class CLASS_DECL_APEX os_context :
+   virtual public object
+{
+public:
 
 
-   class CLASS_DECL_APEX os_context :
-      virtual public object
-   {
-   public:
-      
-
-      os_context();
-      ~os_context() override;
+   os_context();
+   ~os_context() override;
 
 
-      virtual string get_command_line();
+   virtual string get_command_line();
 
-      virtual bool reboot();
-      virtual bool shutdown(bool bPowerOff);
+   virtual bool reboot();
+   virtual bool shutdown(bool bPowerOff);
 
-      virtual void terminate_processes_by_title(const ::string & lpszName);
+   virtual void terminate_processes_by_title(const ::string & strName);
 
 //#ifdef WINDOWS
 //      virtual ::file::path get_module_path(HMODULE hmodule);
 //#endif
 
-      virtual bool get_pid_by_path(const ::string & lpszName, u32 & dwPid);
-      virtual bool get_pid_by_title(const ::string & lpszName, u32 & dwPid);
-      virtual void get_all_processes(u32_array & dwa);
-      virtual ::file::path get_process_path(u32 dwPid);
+   virtual bool get_pid_by_path(const ::string & strName, u32 & dwPid);
+   virtual bool get_pid_by_title(const ::string & strName, u32 & dwPid);
+   virtual void get_all_processes(u32_array & dwa);
+   virtual ::file::path get_process_path(u32 dwPid);
 
-      virtual int get_pid();
+   virtual int get_pid();
 
-      virtual ::payload connection_settings_get_auto_detect();
-      virtual ::payload connection_settings_get_auto_config_url();
+   virtual ::payload connection_settings_get_auto_detect();
+   virtual ::payload connection_settings_get_auto_config_url();
 
-      virtual ::e_status link_open(const string & strUrl, const string & strProfile);
+   virtual ::e_status link_open(const string & strUrl, const string & strProfile);
 
-      virtual bool local_machine_set_run(const ::string & pszKey, const ::string & pszCommand, bool bSet);
-      virtual bool local_machine_set_run_once(const ::string & pszKey, const ::string & pszCommand, bool bSet);
-      virtual bool current_user_set_run(const ::string & pszKey, const ::string & pszCommand, bool bSet);
-      virtual bool current_user_set_run_once(const ::string & pszKey, const ::string & pszCommand, bool bSet);
+   virtual bool local_machine_set_run(const ::string & strKey, const ::string & strCommand, bool bSet);
+   virtual bool local_machine_set_run_once(const ::string & strKey, const ::string & strCommand, bool bSet);
+   virtual bool current_user_set_run(const ::string & strKey, const ::string & strCommand, bool bSet);
+   virtual bool current_user_set_run_once(const ::string & strKey, const ::string & strCommand, bool bSet);
 
-      virtual bool defer_register_ca2_plugin_for_mozilla();
-
-
-      virtual bool file_extension_get_open_with_list_keys(string_array & straKey, const ::string & pszExtension);
-      virtual bool file_extension_get_open_with_list_commands(string_array & straCommand, const ::string & pszExtension);
-
-      virtual bool file_association_set_default_icon(const ::string & pszExtension, const ::string & pszExtensionNamingClass, const ::string & pszIconPath);
-      virtual bool file_association_set_shell_open_command(const ::string & pszExtension, const ::string & pszExtensionNamingClass, const ::string & pszCommand, const ::string & pszParam);
-      virtual bool file_association_get_shell_open_command(const ::string & pszExtension, string & strExtensionNamingClass, string & strCommand, string & strParam);
-
-      virtual bool native_full_web_browser(const ::string & pcsz);
+   virtual bool defer_register_ca2_plugin_for_mozilla();
 
 
-      virtual bool native_modern_web_browser(const ::string & pcsz);
+   virtual bool file_extension_get_open_with_list_keys(string_array & straKey, const ::string & strExtension);
+   virtual bool file_extension_get_open_with_list_commands(string_array & straCommand, const ::string & strExtension);
+
+   virtual bool file_association_set_default_icon(const ::string & strExtension, const ::string & strExtensionNamingClass, const ::string & strIconPath);
+   virtual bool file_association_set_shell_open_command(const ::string & strExtension, const ::string & strExtensionNamingClass, const ::string & strCommand, const ::string & strParam);
+   virtual bool file_association_get_shell_open_command(const ::string & strExtension, string & strExtensionNamingClass, string & strCommand, string & strParam);
+
+   virtual bool native_full_web_browser(const ::string & str);
 
 
-      virtual ::e_status enable_service();
+   virtual bool native_modern_web_browser(const ::string & str);
 
-      virtual ::e_status disable_service();
 
-      virtual ::e_status start_service();
+   virtual ::e_status enable_service();
 
-      virtual ::e_status stop_service();
+   virtual ::e_status disable_service();
 
-      virtual bool enable_service(const ::string & strServiceName, const ::string & strDisplayName, const ::string & strCommand, const ::string & strUser = "", const ::string & strPass = "");
+   virtual ::e_status start_service();
 
-      virtual bool disable_service(const ::string & strServiceName);
+   virtual ::e_status stop_service();
 
-      virtual bool start_service(const ::string & strServiceName);
+   virtual bool enable_service(const ::string & strServiceName, const ::string & strDisplayName, const ::string & strCommand, const ::string & strUser = "", const ::string & strPass = "");
 
-      virtual bool stop_service(const ::string & strServiceName);
+   virtual bool disable_service(const ::string & strServiceName);
+
+   virtual bool start_service(const ::string & strServiceName);
+
+   virtual bool stop_service(const ::string & strServiceName);
 
 //#ifdef WINDOWS
 //      virtual DECLSPEC_NO_RETURN void raise_exception(u32 dwExceptionCode, u32 dwExceptionFlags = EXCEPTION_NONCONTINUABLE);
 //#endif
 
-      virtual bool is_remote_session();
+   virtual bool is_remote_session();
 
-      virtual void set_file_status(const ::string & pszFileName, const ::file::file_status& status);
-
-
-      virtual bool resolve_link(::file::path & path, const ::string & strSource, string * pstrDirectory = nullptr, string * pstrParams = nullptr);
-
-      virtual bool initialize_wallpaper_fileset(::file::set* pfileset, bool bAddSearch);
-
-      virtual bool file_open(::file::path path, string strParams = "", string strFolder = "");
-
-      virtual string get_default_browser();
-
-      virtual bool get_default_browser(string & strId, ::file::path & path, string & strParam);
-
-      virtual bool set_default_browser();
-
-      virtual bool add_default_program(string_array & straExtension, string_array & straMimeType);
-
-      virtual bool register_user_auto_start(string strId, string strCommand, bool bRegister);
-
-      virtual bool is_user_auto_start(string strId);
-
-      virtual ::file::path get_app_path(const ::string & strApp);
-
-      virtual void on_process_command(::create * pcommand);
-
-      /// set["file_filter_specs"] : string array of file extensions (with dot)
-      /// set["file_filter_names"] : string array of the file extensions titles
-      /// set["default_file_extension"] : default file extension (with dot)
-      /// set["folder"] : folder path
-      /// set["file_name"] : in/out file name
-      virtual bool browse_file_open(property_set & set);
-
-      /// set["file_filter_specs"] : string array of extensions (with dot)
-      /// set["file_filter_names"] : string array of the file extensions titles
-      /// set["default_file_extension"] : default file extension (with dot)
-      /// set["folder"] : folder path
-      /// set["file_name"] : in/out file name
-      virtual bool browse_file_save(property_set & set);
-
-      /// set["folder"] : in/out folder path
-      virtual bool browse_folder(property_set & set);
-
-      /// set["folder"] : in/out folder path
-      virtual bool browse_file_or_folder(property_set & set);
+   virtual void set_file_status(const ::string & strFileName, const ::file::file_status& status);
 
 
-      virtual void list_process(::file::patha & patha, u32_array & uaPid);
+   virtual bool resolve_link(::file::path & path, const ::string & strSource, string * pstrDirectory = nullptr, string * pstrParams = nullptr);
+
+   virtual bool initialize_wallpaper_fileset(::file::set* pfileset, bool bAddSearch);
+
+   virtual bool file_open(::file::path path, string strParams = "", string strFolder = "");
+
+   virtual string get_default_browser();
+
+   virtual bool get_default_browser(string & strId, ::file::path & path, string & strParam);
+
+   virtual bool set_default_browser();
+
+   virtual bool add_default_program(string_array & straExtension, string_array & straMimeType);
+
+   virtual bool register_user_auto_start(string strId, string strCommand, bool bRegister);
+
+   virtual bool is_user_auto_start(string strId);
+
+   virtual ::file::path get_app_path(const ::string & strApp);
+
+   virtual void on_process_command(::create * pcommand);
+
+   /// set["file_filter_specs"] : string array of file extensions (with dot)
+   /// set["file_filter_names"] : string array of the file extensions titles
+   /// set["default_file_extension"] : default file extension (with dot)
+   /// set["folder"] : folder path
+   /// set["file_name"] : in/out file name
+   virtual bool browse_file_open(property_set & set);
+
+   /// set["file_filter_specs"] : string array of extensions (with dot)
+   /// set["file_filter_names"] : string array of the file extensions titles
+   /// set["default_file_extension"] : default file extension (with dot)
+   /// set["folder"] : folder path
+   /// set["file_name"] : in/out file name
+   virtual bool browse_file_save(property_set & set);
+
+   /// set["folder"] : in/out folder path
+   virtual bool browse_folder(property_set & set);
+
+   /// set["folder"] : in/out folder path
+   virtual bool browse_file_or_folder(property_set & set);
 
 
-      ::file::patha list_process_path();
-
-      //virtual ::icon_transport load_icon(const ::payload & payloadFile);
-
-      //virtual void enum_draw2d_fonts(::write_text::font_enumeration_item_array& itema);
-
-      virtual ::e_status broadcast_environment_variable_change();
+   virtual void list_process(::file::patha & patha, u32_array & uaPid);
 
 
-   };
+   ::file::patha list_process_path();
 
-   //typedef pointer < os > os_pointer;
+   //virtual ::icon_transport load_icon(const ::payload & payloadFile);
 
+   //virtual void enum_draw2d_fonts(::write_text::font_enumeration_item_array& itema);
 
-//} // namespace apex
-//
-//
-//#endif // AXIS_AXIS_CORE_OS_H
-//
-//
+   virtual ::e_status broadcast_environment_variable_change();
 
 
-
-
-
+};
 
 
 
